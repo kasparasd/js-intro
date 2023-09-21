51d87f5c8330ab0167c5ac2d9e7b913cd14b4b92
Kasparas <kasparas.dapsys@gmail.com>
2023-09-21 08:50:56 +0300

    Infinity and NaN

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -17,5 +17,16 @@ console.log(amzius);
 console.log(pinigine);
 console.log(minusas);
 
+
 // Runint koda i terminal node ./kintamieji/number.js
 
+const begalybe = Infinity;
+const minusBegalybe = -Infinity;
+
+console.log(begalybe, minusBegalybe);
+
+const n = NaN;
+console.log(n, NaN);
+
+const liekana = 7 % 5;
+console.log(liekana);
---
+++ kintamieji/string.js
@@ -0,0 +1,5 @@
+const txt1 = 'text 1'
+const txt2 = "text 2"
+
+console.log(txt1);
+console.log(txt2);
