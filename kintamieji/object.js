3a4ce569e46ac0ae8f2d96099535632c79d9cbd4
Kasparas <kasparas.dapsys@gmail.com>
2023-09-26 10:01:22 +0300

    object end

--- kintamieji/object.js
+++ kintamieji/object.js
@@ -10,7 +10,7 @@ const jonas = {
 
 const maryte = {
     get name(){
-        return 'Maryte';
+        return 'Maryte ';
     },
     age: 88, 
     isMarried:false
@@ -25,3 +25,44 @@ const jonasMarried = jonas.isMarried;
 const maryteName = maryte.name;
 const maryteAge = maryte.age;
 const maryteMarried = maryte.isMarried;
+
+let a = 1;
+let b = a;
+console.log(a,b);
+
+a=2;
+console.log(a,b);
+
+b=3;
+console.log(a,b);
+
+const c = [1,2];
+const d = c;
+
+console.log(c,d);
+
+c[0] = 111;
+console.log(c,d);
+
+c[1] = 222;
+console.log(c,d);
+
+d[0] = 333;
+console.log(c,d);
+
+d[1] = 444;
+console.log(c,d);
+
+const sheeps = [1,2,3];
+const clonedSheeps = [...sheeps];
+
+console.log(sheeps, clonedSheeps);
+
+clonedSheeps[0]=9;
+console.log(sheeps, clonedSheeps);
+
+sheeps.push(8);
+console.log(sheeps, clonedSheeps);
+
+clonedSheeps.push(7);
+console.log(sheeps, clonedSheeps);
