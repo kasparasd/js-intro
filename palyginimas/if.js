5c8dd9d11bd386e8c2773c9bca1e60695bd165ad
Kasparas <kasparas.dapsys@gmail.com>
2023-09-27 16:00:50 +0300

    if

--- palyginimas/if.js
+++ palyginimas/if.js
@@ -1,21 +1,4 @@
 console.clear();
-
-function skaicius(number){
-    if(number%2===0 && number>=0){
-        return `${number} yra lyginis skaicius`
-    } else if(number%2 === 0&& number<0){
-        return `${number} yra neigiamas lyginis skaicius`
-    } else if(number%2 !=0 && number>0){
-        return `${number} yra teigiamas nelyginis skaicius`
-    } else if(number%2 !=0 && number<0){
-        return `${number} yra neigiamas nelyginis skaicius`
-    }
-}
-
-console.log(skaicius(0));
-
-
-
 const abc = {
     raide:'a'
 }
@@ -29,3 +12,33 @@ if(abc.b === 'a') {
 if(abc[b] === 'a') {
     console.log(666); // log
 }
+
+
+// if uzduotis
+// 0 irgi yra lyginis skaicius!
+// 4 yra lyginis skaicius.
+// -4 yra neigiamas lyginis skaicius.
+// 3 nera lyginis skaicius.
+// -3 nera neigiamas lyginis skaicius.
+// 3.14 nera lyginis skaicius, nes desimtainiai skaiciai niekada nebuna lyginiai.
+// -3.14 nera lyginis skaicius, nes desimtainiai skaiciai niekada nebuna lyginiai.
+
+const number = -3.14;
+
+if(number === 0){
+    return console.log(`${number} irgi yra lyginis skaicius`)
+} else if (number % 2 === 0 && number > 0){
+    return console.log(`${number} yra lyginis skaicius`)
+} else if (number % 2 === 0 && number < 0){
+    return console.log(`${number} yra neigiamas lyginis skaicius`)
+} else if (number % 1 === 0 && number > 0){
+    return console.log(`${number} nera lyginis skaicius`)
+} else if (number % 1 === 0 && number < 0){
+    return console.log(`${number} yra neigiamas nelyginis skaicius`)
+} else if (number % 1 !== 0 && number > 0) {
+    return console.log(`${number} nera lyginis skaicius, nes desimtainiai skaiciai niekada nebuna lyginiai.`);
+} else if (number % 1 !== 0 && number < 0) {
+    return console.log(`${number} nera lyginis skaicius, nes desimtainiai skaiciai niekada nebuna lyginiai.`);
+}
+
+
