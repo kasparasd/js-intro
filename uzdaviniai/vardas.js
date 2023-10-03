ec1421d68da0e74bf3e5fda511c17cf2eda2131c
Kasparas <kasparas.dapsys@gmail.com>
2023-10-03 11:26:30 +0300

    vardas uzduotis

---
+++ uzdaviniai/vardas.js
@@ -0,0 +1,22 @@
+console.clear();
+
+function vardas(vardas){
+    const simboliai = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+
+    if(vardas[0] === vardas[0].toUpperCase() && vardas.slice(1) === vardas.slice(1).toLowerCase()){
+        for(let i = 0; i<vardas.length; i++ ){
+           if(!simboliai.includes(vardas[i])){
+            return `Klaida: simbolis ${vardas[i]} neleistinas`
+           }
+        }
+        return true;
+    } else return 'Klaida: patikrinkite ar vardas prasideda is didziosios raides ir ar varde nera daugiau didziuju raidziu';
+}
+
+console.log(vardas('Jonas'));
+
+// function suma(...skaiciai){
+//     console.log(skaiciai);
+// }
+
+// suma(1,2,3)
